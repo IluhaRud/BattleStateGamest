3698f5ad573387a8ad0b95981d91dc53356abd43
IluhaRud <redl2001@outlook.com>
2021-01-20 14:51:59 +0300

    1

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] MainCar mainCar;
+
+    public float downPositionX;
+    public float upPositionX;
+    public float deltaX;
+    public float minDeltaX = 100;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            downPositionX = Input.mousePosition.x;
+        if (Input.GetMouseButtonUp(0))
+        {
+            upPositionX = Input.mousePosition.x;
+            Swipe();
+        }
+    }
+
+    public void Swipe()
+    {
+        deltaX = downPositionX - upPositionX;
+
+        
+    }
+}
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] Transform endOfArea;
+
+    [SerializeField] List<GameObject> areas;
+
+    [SerializeField] GameObject backArea;
+    [SerializeField] GameObject forwardArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "MainCar")
+        {
+            forwardArea = Instantiate(areas[Random.Range(0, areas.Count)], endOfArea.position, Quaternion.identity);
+            forwardArea.gameObject.GetComponent<Trigger>().backArea = gameObject;
+
+            if (backArea != null)
+                Destroy(backArea);
+        }
+    }
+}
