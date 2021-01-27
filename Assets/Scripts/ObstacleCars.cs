f4cc38ed4f5a99951f11441f5fa0f513f9b07d86
IluhaRud <redl2001@outlook.com>
2021-01-27 16:41:28 +0300

    3

---
+++ Assets/Scripts/ObstacleCars.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCars : MonoBehaviour
+{
+    public int maxCars;
+    public int minDistance;
+
+    public Vector2 position;
+
+    int line;
+
+    public List<GameObject> cars;
+
+    private void Start()
+    {
+        for (int i = 0; i < maxCars; i++)
+        {
+            line = Random.Range(0, 2);
+        }
+    }
+
+    void CreateRightCar()
+    {
+
+    }
+
+    void CreateLeftCar()
+    {
+
+    }
+}
